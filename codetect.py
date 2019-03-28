84eb0210393086b60bce7c8105de06218dfbdf95
gunnkrub <32711491+gunnkrub@users.noreply.github.com>
2019-03-28 13:49:40 +0700

    Add files via upload
    Add bullet class

--- codetect.py
+++ codetect.py
@@ -1,1 +1,9 @@
-##def check_player_on_block(player_x, player_y, block_x, block_y):
+def spritecollide(player_x, player_y, block_list):
+        block_hit_list = []
+        for block_x,block_y in block_list:
+                if player_y in range(block_y - ((40//2) + (80//2)), block_y + ((40//2) + (80//2))):
+                        if (block_x - 20 <= player_x - 20 and player_x - 20 <= block_x + 20) or (block_x - 20 <= player_x + 20 and player_x - 20 <= block_x + 20):
+                                block_hit_list.append([block_x,block_y])   
+        return block_hit_list
+
+            
--- models.py
+++ models.py
@@ -1,40 +1,71 @@
 import arcade.key
+from codetect import spritecollide
 class Player:
+    JUMPING_VELOCITY = 15
     GRAVITY = 1
-    JUMPING_VELOCITY = 10
-
     def __init__(self, world, x, y, stage, block_size):
         self.world = world
         self.x = x
         self.y = y
+        self.vx = 0
         self.vy = 0
+        self.jump_status = 0
+        """
+        jump_status = 0 Can double jump
+        jump_status = 1 Jumped 1 time
+        jump_status = 2 Jumped 2 times
+        """
 
         self.stage = stage
 
         self.block_size = block_size
-
-    def update(self, delta):
-        self.y += self.vy
-        self.vy -= Player.GRAVITY
-
+        
     def jump(self):
         self.vy = Player.JUMPING_VELOCITY
+        self.jump_status += 1
 
-    def get_row(self):
-        return (self.y - (self.block_size//2)) // self.block_size
+    def move_player_out_of_block(self, block_hit_list):
+        for block_x, block_y in block_hit_list:
+            if self.y > block_y:
+                if self.vy < 0:
+                    self.y = block_y + (self.block_size * 3 / 2)
+                    self.jump_status = 0
+            elif self.y < block_y:
+                if self.vy > 0:
+                    self.y = block_y - (self.block_size * 3 / 2)
+                    self.vy = 0
+            elif self.x > block_x:
+                if self.vx < 0:
+                    self.x = block_x + (self.block_size * 3 / 2)
+            elif self.x < block_x:
+                if self.vx > 0:
+                    self.x = block_x - (self.block_size * 3 / 2)
 
-    def get_column(self):
-        return self.x // self.block_size
+    def update(self, delta):
+        self.x += self.vx
+        self.y += self.vy
         
-    def get_row_and_column(self):
-        x = get_column()
-        y = get_row()
-        return x,y
+        if self.vy != -10:
+            self.vy -= Player.GRAVITY
 
-    def check_block(self):
-        return [get_column(),get_row()] in self.stage.stage_cr
+        block_hit_list = spritecollide(self.x, self.y, self.stage.block_list)
+        self.move_player_out_of_block(block_hit_list)
 
 
+class Bullet:
+    def __init__(self, world):
+        self.world = world
+        self.player = self.world.player
+        self.x = self.player.x + (self.player.block_size / 2)
+        self.y = self.player.y + 3
+        self.vx = 5
+
+
+    def update(self, delta):
+        self.x += self.vx
+
+
+    
 
 
 class World:
@@ -45,42 +76,76 @@ class World:
         
         self.stage = Stage(self)
         self.player = Player(self, 30, 80, self.stage, self.block_size)
+        self.bullet = []
+
         
     def update(self, delta):
         self.player.update(delta)
+        for bullet in self.bullet:
+            bullet.update(delta)
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.W:
-            self.player.jump()
+            if self.player.jump_status != 2:
+                self.player.jump()
         if key == arcade.key.A:
-            self.player.x -= 10
+            self.player.vx -= 5
         if key == arcade.key.D:
-            self.player.x += 10
+            self.player.vx += 5
+        if key == arcade.key.SPACE:
+            self.bullet.append(Bullet(self))
+
+
+            
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.A or key == arcade.key.D:
+            self.player.vx = 0
+
 
 class Stage:
     def __init__(self,world):
+        self.world = world
         self.map = ['....................',
                     '....................',
                     '....................',
                     '....................',
-                    '................#...',
                     '....................',
-                    '........#...........',
-                    '....................',
+                    '...............####.',
                     '....................',
                     '....................',
-                    '...........#........',
+                    '..#######...........',
+                    '..............####..',
                     '....................',
                     '....................',
-                    '.........#..........',
-                    '####################' ]
+                    '......###...........',
+                    '....................',
+                    '##..................' ]
         self.height = len(self.map)
         self.width = len(self.map[0])
+        self.block_list = []
+        self.write_block_list()
+        
+
+    def get_sprite_position(self, row, column):
+        """ find x,y from column,row
+        row = 0-19 column = 0-14     """
+        x = ((column + 1) * self.world.block_size) - self.world.block_size//2
+        y = (self.world.height - ((row + 1) * self.world.block_size)) + self.world.block_size//2
+        return x,y
+    
+    def write_block_list(self):    
+        for row in range(self.height):
+            for column in range(self.width):
+                if self.has_block(row, column):
+                    self.block_list.append(self.get_sprite_position(row, column))
 
     def has_block(self, row, column):
         return self.map[row][column] == '#'
     def has_blank(self, row, column):
         return self.map[row][column] == '.'
-        
-            
+
+    
+
+
 
